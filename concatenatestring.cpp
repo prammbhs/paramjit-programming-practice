9333920fea82c9311c1ba9306701cf40319e42d5
prammbhs <paramjeetpatelmbhs@gmail.com>
2024-09-03 19:27:08 +0530

    chore: Add code to concatenate two strings

---
+++ concatenatestring.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter your first string: ";
+    string a;
+    cin >> a;
+    cout << "paramEnter your second string: ";
+    string b;
+    cin >> b;
+    cout << "\n Concatenated string is: " << a + b;
+}
